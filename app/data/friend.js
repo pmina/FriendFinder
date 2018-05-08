2d1ef5d8b42414ced618f4ac9a108c0c02839982
pmina <philomn9@gmail.com>
2018-05-08 19:50:19 -0400

    html and api routes

---
+++ app/data/friend.js
@@ -0,0 +1,82 @@
+//array that holds all the "friends" that have taken the survey
+//initialize starting with "dummy" data
+var friendList = [
+    {
+      name: "Mena",
+      profilePic: "https://www.facebook.com/photo.php?fbid=10210556705184408&set=a.1342869102337.2040324.1549500096&type=3&theater",
+      scores: [
+        "4",
+        "5",
+        "5",
+        "3",
+        "4",
+        "5",
+        "4",
+        "2",
+        "3",
+        "4"
+      ]
+    },{
+        name: "Mike",
+        profilePic: "https://www.facebook.com/photo.php?fbid=10155731327310795&set=a.471701690794.275429.684860794&type=3&theater",
+        scores: [
+          "3",
+          "4",
+          "5",
+          "2",
+          "3",
+          "4",
+          "3",
+          "5",
+          "4",
+          "1"
+        ]
+      },{
+        name: "Emad",
+        profilePic: "https://www.facebook.com/photo.php?fbid=10213704751988958&set=a.1581823581397.2079874.1109923537&type=3&theater",
+        scores: [
+          "3",
+          "1",
+          "5",
+          "3",
+          "4",
+          "4",
+          "4",
+          "4",
+          "3",
+          "3"
+        ]
+      },{
+        name: "John",
+        profilePic: "https://www.facebook.com/photo.php?fbid=10155986048106138&set=a.475516711137.284172.732346137&type=3&theater",
+        scores: [
+          "3",
+          "4",
+          "5",
+          "1",
+          "3",
+          "4",
+          "2",
+          "5",
+          "3",
+          "3"
+        ]
+      },{
+        name: "Ray",
+        profilePic: "https://www.facebook.com/photo.php?fbid=10154556478635110&set=a.463141290109.255958.504975109&type=3&theater",
+        scores: [
+          "5",
+          "4",
+          "4",
+          "3",
+          "5",
+          "4",
+          "3",
+          "3",
+          "2",
+          "1"
+        ]
+      }
+  ]
+  
+  module.exports = friendList;
